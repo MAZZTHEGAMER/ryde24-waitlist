160939dcdabe99c3f099f33e57f636bba77eca07
mas'ud <masudhashimb@gmail.com>
2023-08-01 09:29:16 +0100

    we staright now

--- ryde24-waitlist/src/App.jsx
+++ ryde24-waitlist/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 // import Myheader from './components/Header'
 import Home from './components/Home'
 // import Waitlist from './components/Waitlistform'
+// import Goals from './components/Goals'
 
 
 function App() {
   return (
     <>
       <Home/>
+      {/* <Goals/> */}
       {/* <Features/> */}
       
     </>
