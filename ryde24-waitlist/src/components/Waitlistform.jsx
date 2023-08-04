529a1fe7e379bd12e9a550200f313879ba6cc159
mas'ud <masudhashimb@gmail.com>
2023-08-04 12:07:25 +0100

    lets try

--- ryde24-waitlist/src/components/Waitlistform.jsx
+++ ryde24-waitlist/src/components/Waitlistform.jsx
@@ -33,7 +33,7 @@ function Waitlist() {
         <span className="inline-block"></span> Be The First To know when we Lunch 🚀
       </label>
       <div className="relative">
-        <form  name="Waitlistform" method="POST" netlify>
+        <form  name="Waitlistform" action="POST" data-netlify="true">
            <input
           type="email"
           name="waitlist"
