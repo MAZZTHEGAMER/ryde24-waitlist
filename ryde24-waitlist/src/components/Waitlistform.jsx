794fa7168d449a3e6d2a744d9ed7c8662dc8e608
mas'ud <masudhashimb@gmail.com>
2023-08-08 16:11:06 +0100

    same idea, new plan

--- ryde24-waitlist/src/components/Waitlistform.jsx
+++ ryde24-waitlist/src/components/Waitlistform.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
+import {useForm, ValidationError} from '@formspree/react';
 
 function Waitlist() {
   const [waitlist, setWaitlist] = useState("");
   const [showButton, setShowButton] = useState(false);
-
+  
   const showSearchButton = () => {
     setShowButton(true);
   };
-
+  
   const hideSearchButton = () => {
     setShowButton(false);
   };
-
+  
   const Waitlist = () => {
     // Your search logic here
     console.log("Join waitlist:", waitlist);
   };
-
-  // Function to toggle the custom location form
-  // const toggleCustomLocationForm = () => {
-  //   setShowCustomLocation(!showCustomLocation);
-  // };
+  
+  const [state, handleSubmit] = useForm("xaylqjzq");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
 
   return (
     <div className="col-span-full mt-9">
+      <div className="relative">
+        <form
+          name="Waitlistform"
+          method="post"
+          onSubmit={handleSubmit}
+          id="waitlistid"
+        >
       <label
         htmlFor="waitlist"
         className="block text-lg font-semibold leading-6 text-indigo-600 mb-2 "
@@ -31,47 +39,35 @@ function Waitlist() {
         <span className="inline-block"></span> Be The First To know when we
         Lunch 🚀
       </label>
-      <div className="relative">
-        <form
-          name="Waitlistform"
-          method="post"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-          id="waitlistid"
-          
-        >
-          <input type="hidden" name="form-name" value="Waitlistform" />
-          <div>
-            <input name="bot-field" />
-          </div>
-          <div>
-            <label htmlFor="waitlist" type="hidden">
-              📧
-              <br />
-              <input
-                type="email"
-                name="waitlistemails"
-                id="waitlist"
-                autoComplete="off"
-                className="block w-full h-12 rounded-lg border-2 border-indigo-200 px-4 text-gray-900 placeholder-gray-400 focus:outline-none focus:border-indigo-400 transition duration-300 ease-in-out"
-                placeholder="Join The Waitlist? 🚌"
-                value={waitlist}
-                onChange={(e) => {
-                  setWaitlist(e.target.value);
-                  if (e.target.value.trim() !== "") {
-                    showSearchButton();
-                  } else {
-                    hideSearchButton();
-                  }
-                }}
-              />
-            </label>
-          </div>
+          <input
+            type="email"
+            name="waitlist"
+            id="waitlist"
+            autoComplete="off"
+            className="block w-full h-12 rounded-lg border-2 border-indigo-200 px-4 text-gray-900 placeholder-gray-400 focus:outline-none focus:border-indigo-400 transition duration-300 ease-in-out"
+            placeholder="Join The Waitlist? 🚌"
+            value={waitlist}
+            onChange={(e) => {
+              setWaitlist(e.target.value);
+              if (e.target.value.trim() !== "") {
+                showSearchButton();
+              } else {
+                hideSearchButton();
+              }
+            }}
+          />
+           <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+
           {showButton && (
             <button
               className="absolute inset-y-0 right-0 flex items-center pr-4"
-              onClick={Waitlist}
+        
               type="submit"
+              disabled={state.submitting}
             >
               <svg
                 className="h-6 w-6 text-indigo-600"
@@ -94,5 +90,10 @@ function Waitlist() {
     </div>
   );
 }
+// function waitlist() {
+//   return (
+//     <waitlist />
+//   );
+    
 
 export default Waitlist;
